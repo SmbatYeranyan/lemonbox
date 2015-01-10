2964a7abf5f3ae0b36c19d59b753c37e292a66d9
Smbat Yeranyan <smbat.yeranyan@gmail.com>
2015-01-10 23:59:30 +0100

    custom firmware tools and clases

---
+++ lemonBox/esp_iot_sdk_v0.9.2/app/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+main()
+{
+
+  char * temp ="lol some get onboag fsdfdsfs";
+  char * find = "onboarding";
+
+
+    if (strstr(temp,find) != NULL){
+
+printf("Hello, world found!\n");
+
+    }
+return 0;
+}
